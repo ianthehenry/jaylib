316642d8bab49a1ddd987b88c597f7a40e7369f2
Ian Henry <ianthehenry@gmail.com>
2024-07-07 19:05:41 -0700

    add set-shader-value-matrix and set-shader-value-texture

--- src/shader.h
+++ src/shader.h
@@ -142,6 +142,24 @@ static Janet cfun_EndBlendMode(int32_t argc, Janet *argv) {
     return janet_wrap_nil();
 }
 
+static Janet cfun_SetShaderValueMatrix(int32_t argc, Janet *argv) {
+    janet_fixarity(argc, 3);
+    Shader *shader = jaylib_getshader(argv, 0);
+    int locIndex = janet_getinteger(argv, 1);
+    Matrix matrix = jaylib_getmatrix(argv, 2);
+    SetShaderValueMatrix(*shader, locIndex, matrix);
+    return janet_wrap_nil();
+}
+
+static Janet cfun_SetShaderValueTexture(int32_t argc, Janet *argv) {
+    janet_fixarity(argc, 3);
+    Shader *shader = jaylib_getshader(argv, 0);
+    int locIndex = janet_getinteger(argv, 1);
+    Texture2D *texture = jaylib_gettexture2d(argv, 2);
+    SetShaderValueTexture(*shader, locIndex, *texture);
+    return janet_wrap_nil();
+}
+
 static JanetReg shader_cfuns[] = {
     {"load-shader", cfun_LoadShader,
         "(loader-shader vertex-shader fragment-shader)\n\n"
@@ -178,6 +196,14 @@ static JanetReg shader_cfuns[] = {
     {"end-blend-mode", cfun_EndBlendMode,
         "(end-blend-mode)\n\n"
         "End blending mode (reset to default: alpha blending)"
+    },
+    {"set-shader-value-matrix", cfun_SetShaderValueMatrix,
+        "(set-shader-value-matrix shader loc-index matrix)\n\n"
+        "Set 4x4 matrix shader uniform"
+    },
+    {"set-shader-value-texture", cfun_SetShaderValueTexture,
+        "(set-shader-value-matrix shader loc-index texture)\n\n"
+        "Set sampler2d shader uniform"
     },
     {NULL, NULL, NULL}
 };
